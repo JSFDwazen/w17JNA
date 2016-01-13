ee838a1dc2e8b44ad901b1f569ae35d59e04d541
Kamil <cgstyle@gmail.com>
2016-01-13 11:01:59 +0100

    Opdracht 1 klaar

--- src/week17jna/Week17JNA.java
+++ src/week17jna/Week17JNA.java
@@ -11,11 +11,30 @@ package week17jna;
  */
 public class Week17JNA {
 
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // Kamil je moeder
+        
+        SystemTime systemTime = new SystemTime();
+        IGetTime.INSTANCE.GetSystemTime (systemTime);
+        System.out.println(systemTime.toString());
+        
+        IGetTime.INSTANCE.GetSystemTime (systemTime);
+        int begin = systemTime.inMilliseconden();
+        for(int i = 0; i< 1000000000; i++);       
+        IGetTime.INSTANCE.GetSystemTime (systemTime);
+        int eind = systemTime.inMilliseconden();
+        int resultaat = eind - begin;
+        System.out.println("JNA resultaat: " + resultaat);
+        
+        long beginNano = System.nanoTime();
+        for(int i = 0; i< 1000000000; i++);
+        long eindNano = System.nanoTime();
+        int resultaatNano = (int) (eindNano / 1000000 - beginNano / 1000000);
+        System.out.println("Java liP resultaat: " + resultaatNano);
     }
     
 }
